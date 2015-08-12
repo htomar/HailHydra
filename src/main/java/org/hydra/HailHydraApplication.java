6abf7c702e61ab92636d7a236c1bb6a68537ad22
Himanshu Tomar <htomar@sapient.com>
2015-08-12 12:57:09 +0530

    Initial Version

---
+++ src/main/java/org/hydra/HailHydraApplication.java
@@ -0,0 +1,12 @@
+package org.hydra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HailHydraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HailHydraApplication.class, args);
+    }
+}
