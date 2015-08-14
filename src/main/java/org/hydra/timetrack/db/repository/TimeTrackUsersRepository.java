a672c7f9c1c4749d3e4c4cf232aded1ddc61ceb0
Himanshu Tomar <htomar@sapient.com>
2015-08-14 12:30:18 +0530

    Innovation page.

--- src/main/java/org/hydra/timetrack/db/repository/TimeTrackUsersRepository.java
+++ src/main/java/org/hydra/timetrack/db/repository/TimeTrackUsersRepository.java
@@ -1,8 +1,12 @@
 package org.hydra.timetrack.db.repository;
 
+import java.util.List;
+
 import org.hydra.timetrack.db.beans.TimeTrackUsers;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface TimeTrackUsersRepository extends
 		PagingAndSortingRepository<TimeTrackUsers, String> {
+
+	List<TimeTrackUsers> findByFirstNameLike(String firstName);
 }
--- src/main/java/org/hydra/web/rest/controller/TimeTrackingUserController.java
+++ src/main/java/org/hydra/web/rest/controller/TimeTrackingUserController.java
@@ -7,6 +7,7 @@ import org.hydra.timetrack.db.beans.TimeTrackUsers;
 import org.hydra.timetrack.db.repository.TimeTrackUsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +18,12 @@ public class TimeTrackingUserController {
 	@RequestMapping("/users")
 	public List<TimeTrackUsers> getUsers() {
 		List<TimeTrackUsers> usersList = new ArrayList<TimeTrackUsers>();
+		TimeTrackUsers newUser = new TimeTrackUsers();
+		newUser.setUserId("htomar");
+		newUser.setFirstName("Himanshu");
+		newUser.setLastName("Tomar");
+		newUser.setTeam("Hydra");
+		timeTrackUsersRepository.save(newUser);
 		Iterable<TimeTrackUsers> users = timeTrackUsersRepository.findAll();
 		for (TimeTrackUsers user : users) {
 			System.out.println(user.getFirstName());
@@ -24,4 +31,18 @@ public class TimeTrackingUserController {
 		}
 		return usersList;
 	}
+
+	public @RequestMapping("/searchuser") TimeTrackUsers searchUser(
+			@RequestParam(value = "user", required = true) String userParam) {
+		List<TimeTrackUsers> users = timeTrackUsersRepository
+				.findByFirstNameLike(userParam.toLowerCase());
+
+		TimeTrackUsers user = null;
+		if (null != users && !users.isEmpty()) {
+			user = users.get(0);
+		}
+
+		return user;
+	}
+
 }
