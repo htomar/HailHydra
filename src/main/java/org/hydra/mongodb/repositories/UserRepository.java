6465b97d6cb2342ee347d39f35550d01bc3819b3
Himanshu Tomar <htomar@sapient.com>
2015-09-29 12:30:11 +0530

    Changes for resolving compilation issues.

--- src/main/java/org/hydra/mongodb/repositories/UserRepository.java
+++ src/main/java/org/hydra/mongodb/repositories/UserRepository.java
@@ -1,13 +1,11 @@
 package org.hydra.mongodb.repositories;
 
-import org.hydra.mongodb.model.Person;
 import org.hydra.mongodb.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 
-
 @Component
-public interface UserRepository extends MongoRepository<User, String>{
+public interface UserRepository extends MongoRepository<User, String> {
 
 	public User findByEmailID(String emailID);
 
